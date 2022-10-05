e0801e11b05ec429f3dc3d97b9fd6612bbf47fb6
bribrie <ibriebriee@gmail.com>
2022-10-05 09:39:05 +0900

    feat: Add Auth/Price routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import SignInPage from "pages/auth/SignInPage";
+import SignUpPage from "pages/auth/SignUpPage";
+import MainPage from "pages/MainPage";
+import Layout from "components/layout/Layout";
+import AuthLayout from "components/layout/AuthLayout";
+import PricePage from "pages/price/PricePage";
+import PriceCreatePage from "pages/price/PriceCreatePage";
+import "styles/App.scss";
 
 function App() {
+  //TODO: 로그인 유저 확인하기 -> 없으면 바로 로그인 페이지로 보내기
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/price/*" element={<PricePage />}></Route>
+        <Route path="/price/create" element={<PriceCreatePage />} />
+      </Route>
+
+      <Route element={<AuthLayout />}>
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 

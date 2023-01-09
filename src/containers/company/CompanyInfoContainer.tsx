e211bf1722ed2dcc86fbe079b5464aa2bd573b00
bribrie <ibriebriee@gmail.com>
2023-01-09 12:31:22 +0900

    feat: Add loading spinner in company page

--- src/containers/company/CompanyInfoContainer.tsx
+++ src/containers/company/CompanyInfoContainer.tsx
@@ -2,12 +2,14 @@ import { useEffect } from "react";
 import {
   companyInfo,
   companyLinkInfo,
+  companyLoading,
   companyTimeInfo,
   getCompanyAsync,
 } from "redux/company/companySlice";
 import { useAppDispatch, useAppSelector } from "redux/hooks";
 import { currentUserUid } from "redux/auth/authSlice";
 import CompanyInfo from "components/company/CompanyInfo";
+import Loading from "components/layout/Loading";
 
 const CompanyInfoContainer = () => {
   const companyInformation = useAppSelector(companyInfo);
@@ -15,12 +17,15 @@ const CompanyInfoContainer = () => {
   const linkInformation = useAppSelector(companyLinkInfo);
   const dispatch = useAppDispatch();
   const userUid = useAppSelector(currentUserUid);
+  const loading = useAppSelector(companyLoading);
 
   useEffect(() => {
     if (companyInformation.length === 0) {
       dispatch(getCompanyAsync({ userUid }));
     }
   }, [companyInformation.length, dispatch, userUid]);
+
+  if (loading === "pending") return <Loading />;
 
   return (
     <CompanyInfo

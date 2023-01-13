b679414df7be282f76b14483d53cb8a235f09fb3
bribrie <ibriebriee@gmail.com>
2023-01-14 00:04:08 +0900

    style: Fix not found page css

--- src/components/layout/NotFound.tsx
+++ src/components/layout/NotFound.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { ReactComponent as Alert } from "assets/images/Alert.svg";
+import { useAppSelector } from "redux/hooks";
+import { currentUser } from "redux/auth/authSlice";
 import Header from "./Header";
 import styles from "styles/layout/NotFound.module.scss";
 
 const NotFound = () => {
+  const user = useAppSelector(currentUser);
+
   return (
     <div className={styles.container}>
       <Header />
@@ -14,10 +18,10 @@ const NotFound = () => {
         <div className={styles.contentWrapper}>
           <div className={styles.content}>
             <div>서비스 이용에 불편을 드려 죄송합니다.</div>
-            <div>요청하신 페이지에 사용 권한이 없습니다.</div>
+            <div>존재하지 않는 페이지입니다.</div>
           </div>
           <button className={styles.button}>
-            <Link to="/signin">로그인 페이지로 이동</Link>
+            <Link to="/">{user ? "홈으로 이동" : "로그인 페이지로 이동"}</Link>
           </button>
         </div>
       </section>
